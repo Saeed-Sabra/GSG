40010c9247ca7470cd7ae931f4e579255fae5530
Saeed Sabra <saeedsabra565@gmail.com>
2023-06-15 21:00:57 +0300

    commits

--- claculater.js
+++ claculater.js
@@ -9,13 +9,23 @@ calcRouter.post(
   [
     body("x").exists().isInt({ min: 1, max: 50 }),
     body("y").exists().isInt({ min: 1, max: 50 }),
-    body("operation").exists().isIn(["+"]),
+    body("operation").exists().isIn(["+", "-", "*", "/"]),
   ],
   validateResultMiddleware,
   (req, res) => {
     const { x, y, operation } = req.body;
-
-    let result = x + y;
+    let result = 0;
+    if (operation === "+") {
+      result = x + y;
+    } else if (operation === "-") {
+      result = x - y;
+    } else if (operation === "*") {
+      result = x * y;
+    } else if (operation === "/") {
+      result = x / y;
+    } else {
+      throw new Error("Invalid Operation");
+    }
 
     res.status(201).json(result);
   }
